e2c593a1db246eea5ac5fb530e74aa61bfdef51b
nelsonrp24 <nelsonrp24@gmail.com>
2023-08-07 16:20:19 +0200

    Developing product implementation

---
+++ main.tf
@@ -0,0 +1,46 @@
+locals {
+  product_name = "product2"
+  client_name  = "fulano"
+  rg_name      = format("%s-%s", local.product_name, local.client_name)
+
+  tags = { "product" = local.product_name,
+    "client" = local.client_name,
+  }
+  location = "westeurope"
+
+}
+
+module "product_a" {
+
+  source      = "git::https://github.com/tf-init-nxp/tf_product_a_implementation.git?ref=v0.1"
+  rg_location = local.location
+  rg_name     = local.rg_name
+  tags        = local.tags
+  key_vault_access_policies = [
+    {
+      azure_ad_group_names    = ["users-app1"]
+      key_permissions         = ["Get", "List"]
+      secret_permissions      = ["Get", "List"]
+      certificate_permissions = ["Get", "Import", "List"]
+      storage_permissions     = ["Backup", "Get", "List", "Recover"]
+    },
+  ]
+
+  postgresql_administrator_login = "postgres"
+
+  postgresql_databases = { "backend" = { charset = "UTF8", collation = "en_US.UTF8" },
+    "hasura" = { charset = "UTF8", collation = "en_US.UTF8" }
+  }
+  postgresql_allowed_cidrs = { "User1" = "10.10.119.12/32", "User2" = "10.10.101.58/32" }
+
+  aks_enable                        = false
+  keyvault_enable                   = false
+  postgresql_flexible_server_enable = false
+  storage_account_enable            = false
+
+  ###
+  #kured_chart_version = "4.6.0"
+  kured_chart_version = "4.5.1"
+
+
+}
