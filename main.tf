5113c187f83aa03980f81c97540f8fec5dc910cf
Nelson <nelsonrp24@users.noreply.github.com>
2023-08-15 11:18:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,15 +16,15 @@ module "product_a" {
   rg_location = local.location
   rg_name     = local.rg_name
   tags        = local.tags
-  key_vault_access_policies = [
-    {
-      azure_ad_group_names    = ["users-app1"]
-      key_permissions         = ["Get", "List"]
-      secret_permissions      = ["Get", "List"]
-      certificate_permissions = ["Get", "Import", "List"]
-      storage_permissions     = ["Backup", "Get", "List", "Recover"]
-    },
-  ]
+  #key_vault_access_policies = [
+  #  {
+  #    azure_ad_group_names    = ["users-app1"]
+  #    key_permissions         = ["Get", "List"]
+  #    secret_permissions      = ["Get", "List"]
+  #    certificate_permissions = ["Get", "Import", "List"]
+  #    storage_permissions     = ["Backup", "Get", "List", "Recover"]
+  #  },
+  #]
 
   postgresql_administrator_login = "postgres"
 
